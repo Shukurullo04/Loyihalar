fd883d127fa5dd506489a223f42c40399a7ce500
Shukurullo <134263042+Shukurullo04@users.noreply.github.com>
2023-12-27 13:19:17 +0500

    Update Tugilgan_kun.py

--- Tugilgan_kun.py
+++ Tugilgan_kun.py
@@ -1,18 +1,3 @@
-# import datetime
-
-# e=[]
-# for vaqt in "yil","oy","kun":
-#  d=int(input("Tugilgan "+vaqt+": "))
-#  e.append(d)
-
-# tugilgan_vaqt = datetime.date(e[0],e[1], e[2])
-# hozirgi_vaqt = datetime.date.today()
-
-# kunlar = (hozirgi_vaqt- tugilgan_vaqt).days
-
-# print("Siz yashagan kunlar:",kunlar)
-
-
 import datetime
 
 a = []
